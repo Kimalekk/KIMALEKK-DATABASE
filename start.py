cc08ccff9d2db2322c73d05960d4f561ed9a897f
Kimalekk <zapasowe.ezz@wp.pl>
2026-03-31 11:59:29 +0200

    Add files via upload

---
+++ start.py
@@ -0,0 +1,21 @@
+import csv
+
+# Twoje pierwsze dane (lista produktów)
+dane = [
+    ["Produkt", "Cena", "Status"],
+    ["Karta Graficzna", 2500, "Wysyłka"],
+    ["Monitor 4K", 1200, "Magazyn"],
+    ["Kabel HDMI", 45, "Wysłano"]
+]
+
+# Tworzenie pliku CSV (to taka uproszczona baza danych)
+with open('moje_dane.csv', mode='w', newline='', encoding='utf-8') as plik:
+    writer = csv.writer(plik)
+    writer.writerows(dane)
+
+print("Sukces! Plik moje_dane.csv został utworzony w Twoim folderze.")
+
+nowy_produkt = input("Podaj nazwę produktu: ")
+nowa_cena = input("Podaj cenę: ")
+
+print(f"Dodano do bazy: {nowy_produkt} za {nowa_cena} PLN")
